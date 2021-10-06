4dbf1ec487de8734bf414366286bcc1a8564b689
SeongMin-K <eo1tks3@naver.com>
2021-10-06 17:57:29 +0900

    Completion Block / Asynchronous Processing

--- CompletionBlock/ViewController.swift
+++ CompletionBlock/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import KRProgressHUD
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(#fileID, #function, "called")
+        
+        KRProgressHUD.show()
+        sayHello(completion: { result in
+            print("컴플레션 블록으로 넘겨 받음: \(result)")
+            KRProgressHUD.showSuccess()
+            
+            self.mainTitle.text = result
+        })
+        
     }
 
+    fileprivate func sayHello(completion: @escaping(String) -> ()) {
+        print(#fileID, #function, "called")
+        
+        // 2초 딜레이 후 Hello! 넘겨줌
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+            completion("Hello!")
+        })
+    }
 
 }
 
